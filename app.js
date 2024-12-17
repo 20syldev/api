7b096ea81178797401c8375326209504718f92ca
20syldev <git@sylvain.pro>
2024-12-17 10:34:59 +0100

    MAJOR: app: Added more formats to /color endpoint

--- app.js
+++ app.js
@@ -257,8 +257,32 @@ app.get('/:version/captcha', (req, res) => {
 // Generate color
 app.get('/:version/color', (req, res) => {
     const r = random.int(0, 255), g = random.int(0, 255), b = random.int(0, 255);
-    res.jsonResponse({ 
+    const cmyk = (() => {
+        const k = 1 - Math.max(r, g, b) / 255, c = (1 - r / 255 - k) / (1 - k) || 0, m = (1 - g / 255 - k) / (1 - k) || 0, y = (1 - b / 255 - k) / (1 - k) || 0;
+        return [c, m, y, k].map(x => x * 100);
+    })();
+    const hsl = (() => {
+        const r1 = r / 255, g1 = g / 255, b1 = b / 255, max = Math.max(r1, g1, b1), min = Math.min(r1, g1, b1), l = (max + min) / 2;
+        if (max === min) return [0, 0, l * 100];
+        const d = max - min, s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        let h = { [r1]: (g1 - b1) / d + (g1 < b1 ? 6 : 0), [g1]: (b1 - r1) / d + 2, [b1]: (r1 - g1) / d + 4 }[max];
+        return [h * 60 % 360, s * 100, l * 100];
+    })();
+    const hsv = (() => {
+        const max = Math.max(r, g, b), min = Math.min(r, g, b), v = max / 255, s = max ? (max - min) / max : 0;
+        let h = max === min ? 0 : { [r]: (g - b) / (max - min), [g]: 2 + (b - r) / (max - min), [b]: 4 + (r - g) / (max - min) }[max];
+        return [h * 60 % 360, s * 100, v * 100];
+    })();
+    const hwb = (() => {
+        const [h] = hsv, whiteness = Math.min(r, g, b) / 255, blackness = 1 - Math.max(r, g, b) / 255;
+        return [h, whiteness * 100, blackness * 100];
+    })();
+    res.jsonResponse({
+        cmyk: `cmyk(${cmyk.map(x => x.toFixed(1)).join('%, ')}%)`,
         hex: `#${[r, g, b].map(x => x.toString(16).padStart(2, '0')).join('')}`,
+        hsl: `hsl(${hsl[0].toFixed(1)}, ${hsl[1].toFixed(1)}%, ${hsl[2].toFixed(1)}%)`,
+        hsv: `hsv(${hsv[0].toFixed(1)}, ${hsv[1].toFixed(1)}%, ${hsv[2].toFixed(1)}%)`,
+        hwb: `hwb(${hwb[0].toFixed(1)}, ${hwb[1].toFixed(1)}%, ${hwb[2].toFixed(1)}%)`,
         rgb: `rgb(${r}, ${g}, ${b})`
     });
 });
