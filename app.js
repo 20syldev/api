3d4fd249ed0f2aa188e2e330589e84dbdd441203
20syldev <git@sylvain.pro>
2025-03-19 14:31:13 +0100

    CLEANUP: Remove whitespaces

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ let contributions, lastFetch = 0, requests = 0, resetTime = Date.now() + 10000;
 
 /**
  * Check the game result of a Tic-Tac-Toe game.
- * 
+ *
  * @param {Array} moves - The moves of the game.
  * @returns {Object} - The result of the game.
  */
@@ -189,7 +189,7 @@ app.get('/', (req, res) => {
         documentation: 'https://docs.sylvain.pro',
         latest: 'https://api.sylvain.pro/latest',
         logs: 'https://api.sylvain.pro/logs',
-        versions: { 
+        versions: {
             v1: 'https://api.sylvain.pro/v1',
             v2: 'https://api.sylvain.pro/v2',
             v3: 'https://api.sylvain.pro/v3'
@@ -320,7 +320,7 @@ app.get('/:version/algorithms', (req, res) => {
         const n = arr.length;
         for (let i = 0; i < n-1; i++) {
             for (let j = 0; j < n-i-1; j++) {
-                if (arr[j] > arr[j + 1]) [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]; 
+                if (arr[j] > arr[j + 1]) [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
             }
         }
         return res.jsonResponse({ answer: arr });
