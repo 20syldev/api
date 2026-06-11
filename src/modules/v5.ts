42c962143318ddeca91397d663d52fa3700ce7a4
20syldev <git@sylvain.sh>
2026-06-11 21:02:00 +0200

    MINOR: modules: Point v5 barrel to overridden modules

--- src/modules/v5.ts
+++ src/modules/v5.ts
@@ -1,11 +1,7 @@
 export {
-    address,
     agent,
-    algorithms,
     avatar,
-    barcode,
     captcha,
-    chat,
     color,
     convert,
     credit,
@@ -25,18 +21,22 @@ export {
     qrcode,
     regex,
     statistics,
-    text,
-    tic_tac_toe,
     time,
     token,
     username,
     validate,
 } from './v4.js';
+export { default as address } from './v5/address.js';
+export * as algorithms from './v5/algorithms.js';
 export { default as asymmetric } from './v5/asymmetric.js';
+export { default as barcode } from './v5/barcode.js';
 export { default as caseConvert } from './v5/case.js';
+export { default as chat } from './v5/chat.js';
 export * as chart from './v5/chart.js';
 export { default as evaluate } from './v5/evaluate.js';
 export { default as jwt } from './v5/jwt.js';
 export * as matrix from './v5/matrix.js';
 export { default as otp } from './v5/otp.js';
 export { default as symmetric } from './v5/symmetric.js';
+export * as text from './v5/text.js';
+export { default as tic_tac_toe } from './v5/tic_tac_toe.js';
